653c2e1059a014328ae3c19cbb0428a182ebf6d9
jefflssantos <email@jeffsantos.com.br>
2024-05-27 01:25:41 -0300

    feat: add user module

---
+++ modules/User/Enums/UserDocumentTypeEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Modules\User\Enums;
+
+enum UserDocumentTypeEnum: int
+{
+    case CPF = 1;
+    case CNPJ = 2;
+}
--- modules/User/Factories/UserFactory.php
+++ modules/User/Factories/UserFactory.php
@@ -1,13 +1,16 @@
 <?php
 
-namespace Database\Factories;
+namespace Modules\User\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Modules\User\Enums\UserAccountTypeEnum;
+use Modules\User\Enums\UserDocumentTypeEnum;
+use Modules\User\Models\User;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\Modules\User\Models\User>
  */
 class UserFactory extends Factory
 {
@@ -15,6 +18,13 @@ class UserFactory extends Factory
      * The current password being used by the factory.
      */
     protected static ?string $password;
+
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<\Illuminate\Database\Eloquent\Model>
+     */
+    protected $model = User::class;
 
     /**
      * Define the model's default state.
@@ -26,6 +36,17 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'account_type' => fake()->randomElement([
+                UserAccountTypeEnum::CUSTOMER->value,
+                UserAccountTypeEnum::RETAILER->value,
+            ]),
+            'document_type' => $documentType = fake()->randomElement([
+                UserDocumentTypeEnum::CPF->value,
+                UserDocumentTypeEnum::CNPJ->value
+            ]),
+            'document_number' => $documentType === UserDocumentTypeEnum::CPF->value
+                ? fake()->unique()->cpf(false)
+                : fake()->unique()->cnpj(false),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
