352f8e24de948c194ec867dc4486068fdce5bfde
jefflssantos <email@jeffsantos.com.br>
2024-05-28 04:32:29 -0300

    feat: add user type check method to user service

---
+++ tests/Feature/Modules/User/Services/UserServiceTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature\Modules\User\Services;
+
+use Modules\User\Enums\UserAccountTypeEnum;
+use Modules\User\Models\User;
+use Modules\User\Services\UserService;
+use Tests\TestCase;
+
+class UserServiceTest extends TestCase
+{
+    public function test_should_check_if_user_is_retailer(): void
+    {
+        $user = User::factory()->create([
+            'account_type' => UserAccountTypeEnum::RETAILER
+        ]);
+
+        $userService = new UserService($user);
+
+        $this->assertTrue($userService->isRetailer());
+    }
+
+    public function test_should_check_if_user_is_not_retailer(): void
+    {
+        $user = User::factory()->create([
+            'account_type' => UserAccountTypeEnum::CUSTOMER
+        ]);
+
+        $userService = new UserService($user);
+
+        $this->assertFalse($userService->isRetailer());
+    }
+}
