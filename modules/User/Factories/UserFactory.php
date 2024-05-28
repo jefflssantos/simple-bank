ce40905603dcf73ba1e8ec288913bb78ffcae851
jefflssantos <email@jeffsantos.com.br>
2024-05-28 04:41:28 -0300

    ci: set-coverage-to-100

--- modules/User/Factories/UserFactory.php
+++ modules/User/Factories/UserFactory.php
@@ -47,19 +47,8 @@ class UserFactory extends Factory
             'document_number' => $documentType === UserDocumentTypeEnum::CPF->value
                 ? fake()->unique()->cpf(false)
                 : fake()->unique()->cnpj(false),
-            'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
